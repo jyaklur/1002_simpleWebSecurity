a7daf8b736682daee22ce72d85d74ab486aac628
Jeelani <jyaklur@rccl.com>
2021-09-09 15:21:18 -0400

    updated with code to bypass security only for 1 URL

--- src/main/java/com/yaklur/websecurity/WebsecurityApplication.java
+++ src/main/java/com/yaklur/websecurity/WebsecurityApplication.java
@@ -2,12 +2,25 @@ package com.yaklur.websecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class WebsecurityApplication {
+public class WebsecurityApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsecurityApplication.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		//http.csrf().disable().httpBasic();
+		http
+				.authorizeRequests().antMatchers("/employees/home").permitAll()
+				.anyRequest().authenticated()
+				.and().formLogin()
+				.and().httpBasic();
 	}
 
 }
