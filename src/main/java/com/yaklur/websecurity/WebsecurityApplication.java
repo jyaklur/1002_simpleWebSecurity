66d2ee1ddc5292df857784763ad1ad50357d1ac8
Jeelani <jyaklur@rccl.com>
2021-09-09 21:40:25 -0400

    added username and password in application.properties

--- src/main/java/com/yaklur/websecurity/WebsecurityApplication.java
+++ src/main/java/com/yaklur/websecurity/WebsecurityApplication.java
@@ -1,7 +1,9 @@
 package com.yaklur.websecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,5 +24,4 @@ public class WebsecurityApplication extends WebSecurityConfigurerAdapter {
 				.and().formLogin()
 				.and().httpBasic();
 	}
-
 }
